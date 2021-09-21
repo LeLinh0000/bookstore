5814baa1620eb20d4008d7456a221d1c4456d096
Linh Le <zz0deadpool0zz@gmail.com>
2021-09-21 20:29:33 +0700

    Refactor code: create router, app/controllers folder

---
+++ src/app/controllers/NewsController.js
@@ -0,0 +1,16 @@
+
+class NewsController {
+
+    // [Get] /news
+    index(req, res) {
+        res.render('news');
+    }
+
+    // [GET] /news/:slug
+    show(req, res) {
+        res.send('NEW DETAIL');
+    }
+
+}
+
+module.exports = new NewsController;
---
+++ src/app/controllers/SiteController.js
@@ -0,0 +1,16 @@
+
+class SiteController {
+
+    // [Get] /
+    index(req, res) {
+        res.render('home');
+    }
+
+    // [GET] /search
+    search(req, res) {
+        res.render('search');
+    }
+
+}
+
+module.exports = new SiteController;
