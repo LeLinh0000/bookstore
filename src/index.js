009e87ffcf6001bf7b2f9b0c35f7206bcdf8f6c3
Linh Le <zz0deadpool0zz@gmail.com>
2022-01-08 20:58:16 +0700

    fix husky install

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const session = require('express-session');
 const flash = require('connect-flash');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const route = require('./routes');
 
