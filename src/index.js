cdf0e43a71d383a08a2fa1a43eb2c3ab99cb4bfd
Linh Le <zz0deadpool0zz@gmail.com>
2022-01-10 21:20:52 +0700

    fix static path when deploy 4

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ const port = process.env.PORT || 3000;
 
 const route = require('./routes');
 
-app.use(express.static('public'));
+app.use('/public', express.static(path.join(__dirname, 'public')));
 
 app.use(
     express.urlencoded({
